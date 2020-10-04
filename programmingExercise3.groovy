59676ba4da2db6fbbd999f65499231b9b3dad651
Mansi Sadhwani <70261477+MansiSadhwani@users.noreply.github.com>
2020-10-04 23:38:04 +0530

    Create programmingExercise3.groovy

---
+++ programmingExercise3.groovy
@@ -0,0 +1,18 @@
+package SDET
+
+class programmingExercise3 {
+
+	static void main(args) {
+		int x = 0
+		int y = 1
+		int i = 10
+		print x
+		
+		i.times
+		{
+			(x,y) = [y,x+y]
+			
+			print ",$x"
+		}
+	}
+}
